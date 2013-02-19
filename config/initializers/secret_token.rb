9d340c4eca109cd4f8d2dd4b3c2516fab67eabca
David Clapper <dclapper@umail.iu.edu>
2013-02-19 14:08:44 -0500

    User Complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Propertymanagement::Application.config.secret_token = 'a015449c24229e8aa4fb5f06b7761bdd61f86972a23e54ccc881662fc57860a1b4f1387cfff681ae94f58f4899b8e7e6024f40594fe522fcad985829e3ff4873'
